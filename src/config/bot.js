69a7d78d0cd2fcd1050a10519385cf96cf8a2165
astrowalkerrefixxy <joyaltruxnomore@gmail.com>
2026-05-01 14:20:23 +0600

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Devoloped By Refixxy",
         // Activity type number (0 = Playing).
         type: 0, 
       },
